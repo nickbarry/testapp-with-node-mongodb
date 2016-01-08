bcbd2dc9be2165e3849fb54a9d59187c055fc5bc
Nick Barry <niko.barry@gmail.com>
2016-01-08 08:51:46 -0800

    Add image model

---
+++ models/index.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Nicholas on 1/8/2016.
+ */
--- server.js
+++ server.js
@@ -3,11 +3,17 @@
  */
 var express = require('express'),
     config = require('./server/configure'),
-    app = express();
+    app = express(),
+    mongoose = require('mongoose');
 
 app.set('port', process.env.PORT || 3300);
 app.set('views', __dirname + '/views');
 app = config(app);
+
+mongoose.connect('mongodb://localhost/imgPloadr');
+mongoose.connection.on('open', function(){
+    console.log('Mongoose connected.');
+});
 
 //app.get('/', function(req, res){
 //    res.send('Hello World');
