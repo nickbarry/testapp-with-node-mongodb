b9f2ee3f269abe7a4d24fc31d31cc8fee7a37942
Nick Barry <niko.barry@gmail.com>
2016-01-10 23:33:05 -0800

    Add comment model and index of models

--- models/comment.js
+++ models/comment.js
@@ -1,3 +1,25 @@
 /**
  * Created by Nicholas on 1/8/2016.
  */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId;
+
+var CommentSchema = new Schema({
+    image_id:   {type: ObjectId},
+    email:      {type: String},
+    name:       {type: String},
+    gravatar:   {type: String},
+    comment:    {type: String},
+    timestamp:  {type: Date, 'default': Date.now}
+});
+
+CommentSchema.virtual('image')
+    .set(function(image){
+        this._image = image;
+    })
+    .get(function(){
+        return this._image;
+    });
+
+module.exports = mongoose.model('Comment', CommentSchema);
--- models/index.js
+++ models/index.js
@@ -1,3 +1,7 @@
 /**
  * Created by Nicholas on 1/8/2016.
  */
+module.exports = {
+    'Image': require('./image'),
+    'Comment': require('./comment')
+};
